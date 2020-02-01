28c3f44bf7fd7891ec7d4dfb0ca01c67676a78c2
MSI\hesmeron <kr201@interia.pl>
2020-02-01 04:08:56 +0100

    Added drag and drop for pears and block prefabs

---
+++ Assets/Pear/DragAndDrop.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (Rigidbody))]
+public class DragAndDrop : MonoBehaviour
+{
+    public bool isDragged = false;
+    public float distance = 5f;
+    public float dragSpeed = 2f;
+    public float maxMagnitude = 10f;
+    public Rigidbody rgb;
+
+    private void Start()
+    {
+        rgb = this.gameObject.GetComponent<Rigidbody>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDragged)
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0, 0, distance));
+            Vector3 heading = mousePosition - transform.position;
+            heading = Vector3.ClampMagnitude(heading * dragSpeed, maxMagnitude);           
+            rgb.velocity = heading;
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        isDragged = true;
+    }
+
+    public void OnMouseUp()
+    {
+        isDragged = false;
+    }
+}
