0ea91a66a03af6b9d9e51e014ba317f5b4102742
Artem <artemsilantyev@gmail.com>
2020-02-01 10:17:07 -0500

    Deleted SaveItem.cs Added key input controls Added a component manager class Added Swipe Control template

---
+++ Assets/Scripts/Inputs/KeyInputs/KeyInputsManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class KeyInputsManager : Singleton<KeyInputsManager>
+{
+    [SerializeField]
+    private KeyInputs[] inputs;
+
+    private void OnValidate()
+    {
+        TryFindAllInputs();
+    }
+
+    private void Start()
+    {
+        TryFindAllInputs();
+    }
+
+    private void TryFindAllInputs()
+    {
+        inputs = GetComponentsInChildren<KeyInputs>();
+    }
+
+    public KeyInputs GetInputForKey(KeyCode key)
+    {
+        return inputs.FirstOrDefault(x => x.GetKeyCode() == key);
+    }
+}
---
+++ Assets/Scripts/Utilities/ComponenetManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ComponenetManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<Component> components;
+
+    private void OnValidate()
+    {
+        GetAllComponents();
+    }
+
+    private void GetAllComponents()
+    {
+        components = new List<Component>(GetComponents<Component>());
+    }
+
+    public bool TryAddComponent(Type type)
+    {
+        if (HasComponent(type))
+        {
+            var component = gameObject.AddComponent(type);
+            components.Add(component);
+            return true;
+        }
+        return false;
+    }
+
+    public bool TryRemoveComponent(Type type)
+    {
+        if (HasComponent(type))
+        {
+            var component = GetComponent(type);
+            components.Remove(component);
+            Destroy(component);
+            return true;
+        }
+        return false;
+    }
+
+    public bool HasComponent(Type type)
+    {
+        return components.FirstOrDefault(x => x.GetType() == type) != null;
+    }
+}
