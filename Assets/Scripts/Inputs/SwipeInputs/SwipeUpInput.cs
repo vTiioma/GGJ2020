e97289581a87a7169a53f0069f9c1373928ebda8
Artem <artemsilantyev@gmail.com>
2020-02-01 10:53:47 -0500

    added swipe controlls added swipe coontrols scene

---
+++ Assets/Scripts/Inputs/SwipeInputs/SwipeUpInput.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SwipeUpInput : SwipeInput
+{
+    protected override bool IsWithinSwipeThreshold(Vector2 position)
+    {
+        float delta = position.y - downPosition.y;
+        if (delta > 0 && Mathf.Abs(delta) >= GetSwipeThreshold())
+        {
+            return true;
+        }
+        return false;
+    }
+}
