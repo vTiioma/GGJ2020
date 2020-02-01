b8c778ac1e0aab15793f4b7a82f460fd0888bcf3
MSI\hesmeron <kr201@interia.pl>
2020-02-01 06:03:37 +0100

    Simpler and more elastic system already implemented but without necessary components yet

--- Assets/Blocks/ChangableObject.cs
+++ Assets/Blocks/ChangableObject.cs
@@ -4,9 +4,33 @@ using UnityEngine;
 
 public class ChangableObject : MonoBehaviour
 {
+    public string objectName = "block";
+    public List<Component> components = new List<Component>();
     // Start is called before the first frame update
     void Awake()
     {
-        this.gameObject.AddComponent<BehaviourChanger>().component = this;
+        Scene.changableObjects.Add(this);
+    }
+
+    public void RemoveComponent(System.Type type)
+    {
+        List<Component> componentsToRemove = new List<Component>();
+        foreach(var c in components)
+        {
+            if(c.GetType() == type)
+            {
+                componentsToRemove.Add(c);
+            }
+        }
+        foreach(var c in componentsToRemove)
+        {
+            components.Remove(c);
+        }
+    }
+
+    public void AddComponent(System.Type type)
+    {
+        Component newComponent = this.gameObject.AddComponent(type);
+        components.Add(newComponent);
     }
 }
---
+++ Assets/Blocks/Scene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Scene
+{
+    public static List<ChangableObject> changableObjects = new List<ChangableObject>();
+
+    public static void RemoveFrom(Component com, string name)
+    {
+        foreach(var c in changableObjects)
+        {
+            if(c.objectName == name)
+            {
+                c.RemoveComponent(com.GetType());
+            }
+        }
+    }
+
+    public static void AddTo(Component com, string name)
+    {
+        foreach (var c in changableObjects)
+        {
+            if (c.objectName == name)
+            {
+                c.AddComponent(com.GetType());
+            }
+        }
+    }
+}
