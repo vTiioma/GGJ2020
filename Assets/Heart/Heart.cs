2a72191d0b77b845b7505133cd5bf6b78539efa2
MSI\hesmeron <kr201@interia.pl>
2020-02-01 04:19:32 +0100

    Adding different behaviours for blocks and winning conditions with victory log

---
+++ Assets/Heart/Heart.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Heart : MonoBehaviour
+{
+    public bool isOneIn = false;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isOneIn)
+        {
+            Victory();
+        }
+        else
+        {
+            isOneIn = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        isOneIn = false;
+    }
+    void Victory()
+    {
+        Debug.Log("You won, Congratulations i guess. Are you happy with yourself now? Did this few endophines made your life a little bit more worth all the hassel? I sure hope so becaiuse that's about it. Jk. Th longest debug log that i ever wrote. This may be the ending screen. I mean it feels right");
+    }
+}
