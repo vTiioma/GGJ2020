91c4ad0fb5121c9786168999a43ff979cbd0d4e4
MSI\hesmeron <kr201@interia.pl>
2020-02-01 23:17:56 +0100

    freeze

---
+++ Assets/Scenes/Freeze.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Freeze : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Start()
+    {
+        StartCoroutine(FreezeAGame());
+    }
+    IEnumerator FreezeAGame()
+    {
+        Time.timeScale = 0.01f;
+        yield return new WaitForSeconds(0.01f);
+        Time.timeScale = 1f;
+    }
+}
