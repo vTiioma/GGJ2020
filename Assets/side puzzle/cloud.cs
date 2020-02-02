2c5f589647a6ad229da00aac08bfe4ca8ea70366
Artem <artemsilantyev@gmail.com>
2020-02-02 12:28:13 -0500

    added cloud functionality

---
+++ Assets/side puzzle/cloud.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cloud : MonoBehaviour
+{
+    private void Start()
+    {
+        transform.position = new Vector2(Random.Range(-10, 10), Random.Range(-3, 2.5f));
+        transform.localScale = new Vector3(Random.Range(0.5f, 1.25f), Random.Range(0.5f, 1.25f), 1);
+        float delta = transform.position.x * 2 / 24;
+        LeanTween.moveX(gameObject, -12, Random.Range(10f, 20f) * delta).setOnComplete(AnimateCloud);
+    }
+
+    private void AnimateCloud()
+    {
+        transform.position = new Vector2(12, transform.position.y);
+        LeanTween.moveX(gameObject, -12, Random.Range(10f, 20f)).setOnComplete(AnimateCloud);
+    }
+}
